04790c399c3ff933c556b71e33659f12203e9b45
faxin <mathiasberglund56@gmail.com>
2020-07-17 00:36:35 +0200

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>PHP Store</title>
+	</head>
+	<body>
+	 <?php
+    // Två sträck = hur man kommentrerar i php.
+	 echo "<h1>Welcome</h>";
+	 ?>
+	</body>
+	</html>
